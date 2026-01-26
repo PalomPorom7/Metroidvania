7b73056c7cf1e60d2377616088499186879cbb5a
Thomas Yanuziello <thomas.yanuziello@gmail.com>
2026-01-26 08:54:04 -0500

    Parallax

--- Scripts/room.gd
+++ Scripts/room.gd
@@ -5,8 +5,8 @@ extends Area2D
 
 
 func get_top_left() -> Vector2:
-	return global_position + _shape_node.shape.get_rect().position
+	return _shape_node.global_position + _shape_node.shape.get_rect().position
 
 
 func get_bottom_right() -> Vector2:
-	return global_position + _shape_node.shape.get_rect().end
+	return _shape_node.global_position + _shape_node.shape.get_rect().end
